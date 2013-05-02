0631512bc7247935c693a7049e2166534ed54b61
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-05-02 17:54:20 +0900

    Add after callback for asset:precompile.

--- assets_precompile.rb
+++ assets_precompile.rb
@@ -8,6 +8,7 @@ namespace :deploy do
     task :precompile, :roles => :web, :except => { :no_release => true } do
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
     end
-
   end
+
+  after "deploy:migrate", "deploy:assets:precompile"
 end
