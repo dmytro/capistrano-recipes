23d59e9c40af44f1187abb158b53565a27cad8a5
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-05-20 19:32:42 +0900

    Use try_sudo instead of sudo.
    - if use_sudo set to false, don't use it. Otherwise get error about missing terminal.

--- nginx.rb
+++ nginx.rb
@@ -5,32 +5,33 @@ set_default(:nginx_chrome_frame, true)
 namespace :nginx do
   desc "Install latest stable release of nginx"
   task :install, roles: :web do
-    run "#{sudo} add-apt-repository ppa:nginx/stable"
-    run "#{sudo} apt-get -y update"
-    run "#{sudo} apt-get -y install nginx"
+    run "#{try_sudo} add-apt-repository ppa:nginx/stable"
+    run "#{try_sudo} apt-get -y update"
+    run "#{try_sudo} apt-get -y install nginx"
   end
   after "deploy:install", "nginx:install"
 
   desc "Setup nginx configuration for this application"
   task :setup, roles: :web do
     template "nginx.conf.erb", "/tmp/nginx_conf"
-    run "#{sudo} mkdir -p #{shared_path}/config"
-    run "#{sudo} cp /tmp/nginx_conf #{shared_path}/config/nginx.conf"
-    run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
-    run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
+    run "#{try_sudo} mkdir -p #{shared_path}/config"
+    run "#{try_sudo} cp /tmp/nginx_conf #{shared_path}/config/nginx.conf"
+    run "#{try_sudo} mkdir -p /etc/nginx/sites-enabled/#{application}"
+    run "#{try_sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
+    run "#{try_sudo} rm -f /etc/nginx/sites-enabled/default"
   end
   after "deploy:setup", "nginx:setup"
 
   %w[start stop].each do |command|
     desc "#{command} nginx"
     task command, roles: :web do
-      run "#{sudo} service nginx #{command}"
+      run "#{try_sudo} service nginx #{command}"
     end
     after "deploy:#{command}", "nginx:#{command}"
   end
 
   desc "Restart nginx"
   task :restart, roles: :web do
-    run "#{sudo} service nginx restart"
+    run "#{try_sudo} service nginx restart"
   end
 end
