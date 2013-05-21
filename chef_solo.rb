db1086ac736f01bcaaea17f02734a69c755c3ac4
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-05-21 11:21:32 +0900

    Change chef-solot to copy and run files using cap, rtther than SSH.

--- chef_solo.rb
+++ chef_solo.rb
@@ -1,16 +1,43 @@
-namespace :chefsolo do 
-  desc <<-EOF
-   Run chef-solo deploy onto the remote server.
+#
+# @author Dmytro Kovalov, dmytro.kovalov@gmail.com
+# 
+set_default :chef_solo_path, File.expand_path("../chef-solo/", File.dirname(__FILE__))
+set_default :chef_solo_json, "empty.json"
 
-   Task needs chef-solo repository installed as git submodule in
-   config/deploy/chef-solo, and uses SSH key authentication.
+namespace :chefsolo do 
+
+  desc <<-EOF
+   Run chef-solo deploy on the remote server.
+
+   Task needs chef-solo repository git@github.com:dmytro/chef-solo.git
+   installed as git submodule or directory.
+
+   Configuration:
+
+    * set :chef_solo_path, <PATH> 
+
+      Path to the directory where, chef-solo is
+      installed. If not defined would search in
+      ./config/deploy/chef-solo
+
+    * set :chef_solo_json, "file.json"
+
+      JSON configuration for Chef solo to deploy. Defaults to
+      empty.json
+
 
 EOF
-  task :deploy, roles: :app do
-    dir = File.expand_path("../chef-solo/", File.dirname(__FILE__))
-    stdout = run_locally %Q{ssh-agent /bin/bash -c "cd #{dir} && ssh-add #{ssh_options[:keys][0]} && CUSTOM_SSH_OPTIONS='-F /dev/null' ./deploy.sh #{user}@#{host}"}
-    puts "OUTPUT:" + stdout
+  task :deploy do
+    temp = %x{ mktemp /tmp/captemp-tar.XXXX }.chomp
+
+    run_locally "cd #{chef_solo_path} && tar cfz #{temp} . "
+    upload( temp, temp, :via => :scp)
+    run_locally "rm -f #{temp}"
+
+    run "mkdir -p ~/chef && cd ~/chef && tar xfz #{temp} && rm -f #{temp}"    
+    run "#{try_sudo} cd ~/chef && bash ./install.sh #{chef_solo_json}"
   end
+
 
   before "deploy:install", "chefsolo:deploy"
 end
