7894fdf34115458bdabae2fc52e13eb501ffd95e
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-06-05 12:44:48 +0900

    Setup task to chown deployment directories

---
+++ setup.rb
@@ -0,0 +1,17 @@
+namespace :deploy do 
+
+  desc <<-DESC
+
+  Change ownership of the created directories at the setup stage to
+  the application deployment user. Run this after deploy:setup task.
+
+  DESC
+  task :chown_dirs, :except => { :no_release => true } do 
+    dirs = [deploy_to, releases_path, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d.split('/').last) }
+    run "#{try_sudo} chown -R #{user} #{dirs.join(' ')}" if user
+  end
+
+  after "deploy:setup", "deploy:chown_dirs"
+  before "database:setup", "deploy:chown_dirs"
+end
