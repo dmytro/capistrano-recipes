4c57468e5cdb95a60c6da884c00a33dfed599aed
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-06-13 13:48:30 +0900

    Don't use install task - chef_solo does a better job

--- base.rb
+++ base.rb
@@ -6,11 +6,3 @@ end
 def set_default(name, *args, &block)
   set(name, *args, &block) unless exists?(name)
 end
-
-namespace :deploy do
-  desc "Install everything onto the server"
-  task :install do
-    run "#{sudo} apt-get -y update"
-    run "#{sudo} apt-get -y install curl git-core python-software-properties"
-  end
-end
