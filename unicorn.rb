73bbf2e351a762c2bef97a19490127140edb46b2
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-06-20 17:09:16 +0900

    Add restart unicorn and reload tasks.

--- unicorn.rb
+++ unicorn.rb
@@ -11,6 +11,7 @@ namespace :unicorn do
 
   start_unicorn  = "(cd #{current_path} && bundle exec unicorn -E production -c #{current_path}/config/unicorn.rb -D)"
   reload_unicorn = "( kill -s USR2 `cat #{unicorn_pid}` || true )"
+  stop_unicorn = "( kill `cat #{unicorn_pid}` || true )"
 
   unicorn_running = "( test -f #{unicorn_pid} && ps $(cat #{unicorn_pid}) > /dev/null ) ; echo $? "
 
@@ -39,6 +40,7 @@ namespace :unicorn do
   end
   after "deploy:stop", "unicorn:stop"
 
+  dsesc "Reload unicorn"
   desc "Restart unicorn"
   task :restart, roles: :app do
 
@@ -47,6 +49,21 @@ namespace :unicorn do
     if running
       logger.info "Reloading Unicorn"
       run reload_unicorn
+    else
+      logger.info "Unicorn is not running. Starting."
+      run start_unicorn
+    end
+  end
+
+  desc "Restart unicorn"
+  task :restart, roles: :app do
+
+    running = ( capture(unicorn_running).strip == '0')
+    
+    if running
+      logger.info "Reloading Unicorn"
+      run stop_unicorn
+      run start_unicorn
     else
       logger.info "Unicorn is not running. Starting."
       run start_unicorn
