3a4c30c25911b58a7ce18fa3566025fb23fb133b
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-08-20 17:49:48 +0900

    Add PTY for sudo use.

--- chef_solo_roles.rb
+++ chef_solo_roles.rb
@@ -21,7 +21,7 @@ EOF
     find_servers_for_task(current_task).each do |server|
       role_names_for_host(server).each do |role|
         file = "#{role.to_s}.json"
-        run  "#{chef_solo_command} #{chef_solo_remote}/#{file}" if File.exists? "#{chef_solo_path}/#{file}"
+        run("#{chef_solo_command} #{chef_solo_remote}/#{file}", :pty => true) if File.exists? "#{chef_solo_path}/#{file}"
       end
     end
   end
