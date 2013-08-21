e06459be72206a05a920c4d209ff99177ecb4a7a
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-08-21 16:28:13 +0900

    Configuration namespace and task to configure sudo.

--- sudo.rb
+++ sudo.rb
@@ -44,10 +44,51 @@ MSG
       end
 
     end
+    
+  end
+
+  namespace :configure do 
+    
+    desc "Check that necessary config exists for sudo and create it if not"
+    task :sudo do 
+      configured = test_command "sudo -l -n"
+      next if configured
+
+      puts <<-MSG
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SUDO is not cofigured for user '#{user}'
+
+Please type root password at the prompt below, 
+we will try to configure it.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+MSG
+
+      dir_exist =  test_command "ls -ld /etc/sudoers.d"
+
+      tmp = capture("mktemp", shell: :bash).strip
+
+      if dir_exist
+        surun "echo \"#{user}   ALL=(ALL) NOPASSWD: ALL\" >  /etc/sudoers.d/deploy_user_#{user}"
+      else
+        surun "cat /etc/sudoers > #{tmp}"
+      
+        run "echo \"#{user}   ALL=(ALL) NOPASSWD: ALL\" >> #{tmp}", shell: :bash
+
+        ok = test_command "visudo -c -f #{tmp}" # Check syntax
+        abort "ATTENTION: Syntax check of sudoers file failed. Please investigate before continuing." unless ok
+        
+        surun "cat #{tmp} > /etc/sudoers"
+
+      end
+      run "rm -f #{tmp}", shell: :bash
+    end
   end
 end
 
+
 before "chefsolo:deploy", "prerequisites:install:sudo"
+after "prerequisites:install:sudo", "prerequisites:configure:sudo"
 
 # TODO: fix it - runs twice
 # before "deploy", "prerequisites:install:sudo" # If chef-solo deploy is
