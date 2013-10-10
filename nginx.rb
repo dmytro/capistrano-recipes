9feb645efde5ad295c4e0880832520d9dd5aa158
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-10-10 17:14:41 +0900

    Configurable PATH's for Nginx log files. Add documentation.

--- nginx.rb
+++ nginx.rb
@@ -2,6 +2,8 @@ set_default(:nginx_port, 80)
 set_default(:nginx_error_pages, true)
 set_default(:nginx_chrome_frame, true)
 set_default(:domain_name, "#{application}")
+set_default(:nginx_access_log, "#{shared_path}/log/nginx_#{application}_access.log")
+set_default(:nginx_error_log, "#{shared_path}/log/nginx_#{application}_error.log")
 
 namespace :nginx do
 
@@ -13,7 +15,30 @@ namespace :nginx do
   # end
   # after "deploy:install", "nginx:install"
 
-  desc "Setup nginx configuration for this application"
+  desc <<-DESC
+Setup nginx configuration for this application.
+
+Confoiguration template is for using Nginx with Unicorn. For other
+uses, this needs to be changed.
+
+Configuration varaibles
+------------------------
+
+- :nginx_port - default 80, port to listen
+
+- :nginx_error_pages
+
+- :nginx_chrome_frame
+
+- :nginx_access_log - PATH to access log file, by default
+  "#{shared_path}/log/nginx_#{application}_access.log"
+
+- :nginx_error_log- PATH to error log file, by default
+  "#{shared_path}/log/nginx_#{application}_error.log"
+
+Source #{path_to __FILE__}
+
+DESC
   task :setup, roles: [:app, :web], except: { no_release: true } do
     template "nginx.conf.erb", "/tmp/nginx_conf"
     run "#{try_sudo} mkdir -p #{shared_path}/config"
@@ -37,7 +62,11 @@ namespace :nginx do
     sudo "service nginx restart"
   end
 
-  desc "Ensure Apache is not running in case it is installed"
+  desc <<-DESC
+Ensure Apache is not running in case it is installed.
+
+Source #{path_to __FILE__}
+DESC
   task :apache_stop, roles: [:app, :web], except: { no_release: true }  do
     %w{ apache2 apache httpd }.each do |serv| # Different names for apache in varios distros
       sudo "service #{serv} stop || true"
