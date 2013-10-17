8f52f63e2ed39006287bf7154c95f6e11bf31819
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-10-17 16:41:10 +0900

    Add hostname entry to /etc/hosts.

--- hostname.rb
+++ hostname.rb
@@ -9,6 +9,8 @@ hosts - it is used for remote logging. Hostnames should be unique.
 
 Supported OS: RHEL/CentOS (etc/sysconfig/network) and Debian/Ubuntu (/etc/hostname).
 
+Note: This tested only on Linux, since in depends on format returned by Linux's ifconfig.
+
 Source: #{path_to __FILE__}
 
 DESC
@@ -17,6 +19,7 @@ DESC
       hostname = current_server.options[:hostname]
       next unless hostname
       run "hostname | grep ^#{hostname}$ > /dev/null || ( #{sudo} hostname #{hostname}; if [ test -d /etc/sysconfig ]; then (echo NETWORKING=yes; echo NETWORKING_IPV6=no; echo HOSTNAME=#{hostname}) | sudo tee /etc/sysconfig/network; else echo #{hostname} | sudo tee /etc/hostname; fi)", :hosts => [current_server.host]
+      run "grep ' #{hostname}$' /etc/hosts || (IP=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_,\" \");print _[1]}') && echo \"$IP #{hostname}\" | sudo tee -a /etc/hosts)", :hosts => [current_server.host]
     end
   end
 end
