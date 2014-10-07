bb6019ebe16ab1fd50ede866c7bdee4b661e0134
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2014-10-07 08:08:17 +0000

    Download secrets databag from s3, using Ruby library.

--- chef_solo_databags.rb
+++ chef_solo_databags.rb
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+
+set_default :s3_secret_databag_path, "/secrets/data_bags/"
+set_default :s3_region, "ap-northeast"
 
 require 'json'
+require "aws/s3"
 
 namespace :chefsolo do
   namespace :databag do
@@ -147,15 +152,51 @@ Configuration:
    set :use_s3_secrets, true
 
 
-- Set URI to access data_bags on S3:
+- Set path to access data_bags on S3:
 
-   set :s3_secret_databag_path, "s3://secrets/data_bags/"
+   set :s3_secret_databag_path, "/secrets/data_bags/"
+
+- Set S3 region:
+
+   set :s3_region, "ap-northeast"
+
+S3 credentials
+--------------
+
+To use this recipe create file `config/secrets/s3.yml` with follwing
+content:
+
+    ---
+    access_key_id: 'KEYID'
+    secret_access_key: 'SECRET_ACCESS_KEY'
+
 
 Source File #{path_to __FILE__}
 
 EOF
     task :secrets do
-      run "s3cmd get --recursive --skip-existing --no-progress #{s3_secret_databag_path} #{local_chef_cache_dir}/ > /tmp/s3cmd.log"
+
+      keys = YAML.load_file("config/secrets/s3.yml")
+
+      conn = AWS::S3::Base.establish_connection!(
+        :access_key_id => keys["access_key_id"],
+        :secret_access_key => keys["secret_access_key"]
+      )
+      AWS::S3::DEFAULT_HOST.replace "s3-#{s3_region}-1.amazonaws.com"
+
+      bucket = AWS::S3::Bucket.find(s3_secret_databag_path.split("/")[1])
+
+      bucket.each do |file|
+
+        path = file.path
+        next unless path.slice! s3_secret_databag_path
+        dir = FileUtils.mkdir_p("#{local_chef_cache_dir}/#{File.dirname(path)}")
+
+        File.open("#{local_chef_cache_dir}/#{path}", "w") do |f|
+          f.write file.value
+          f.close
+        end
+      end
     end
   end                           # namespace databag
 end                             # namespace chefsolo
